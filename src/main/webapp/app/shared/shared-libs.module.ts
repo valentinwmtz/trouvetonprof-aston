5836aca88c521870c68f73095a11bb3eba158a75
Unknown <wimetz.valentin@hotmail.com>
2019-02-03 15:10:08 +0100

    add material angular

--- src/main/webapp/app/shared/shared-libs.module.ts
+++ src/main/webapp/app/shared/shared-libs.module.ts
@@ -6,10 +6,28 @@ import { NgJhipsterModule } from 'ng-jhipster';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { CookieModule } from 'ngx-cookie';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JhMaterialModule } from 'app/shared/material/jh-material.module';
 
 @NgModule({
-    imports: [NgbModule.forRoot(), InfiniteScrollModule, CookieModule.forRoot(), FontAwesomeModule],
-    exports: [FormsModule, CommonModule, NgbModule, NgJhipsterModule, InfiniteScrollModule, FontAwesomeModule]
+    imports: [
+        NgbModule.forRoot(),
+        InfiniteScrollModule,
+        CookieModule.forRoot(),
+        FontAwesomeModule,
+        BrowserAnimationsModule,
+        JhMaterialModule
+    ],
+    exports: [
+        FormsModule,
+        CommonModule,
+        NgbModule,
+        NgJhipsterModule,
+        InfiniteScrollModule,
+        FontAwesomeModule,
+        BrowserAnimationsModule,
+        JhMaterialModule
+    ]
 })
 export class TrouvetonprofSharedLibsModule {
     static forRoot() {
