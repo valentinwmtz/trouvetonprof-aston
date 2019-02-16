e2160a888de3fbe2a7587dcea0771a49af21b972
Unknown <wimetz.valentin@hotmail.com>
2019-02-16 09:30:18 +0100

    update file stackoverflow

--- src/main/java/com/trouvetonprof/config/DatabaseConfiguration.java
+++ src/main/java/com/trouvetonprof/config/DatabaseConfiguration.java
@@ -1,20 +1,23 @@
 package com.trouvetonprof.config;
 
 import io.github.jhipster.config.JHipsterConstants;
-import io.github.jhipster.config.h2.H2ConfigurationHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import io.github.jhipster.config.liquibase.AsyncSpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.core.task.TaskExecutor;
+import io.github.jhipster.config.liquibase.AsyncSpringLiquibase;
+import liquibase.integration.spring.SpringLiquibase;
 
-import java.sql.SQLException;
-import java.lang.NumberFormatException;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("com.trouvetonprof.repository")
@@ -26,35 +29,30 @@ public class DatabaseConfiguration {
 
     private final Environment env;
 
-    public DatabaseConfiguration(Environment env) {
+    private final CacheManager cacheManager;
+
+    public DatabaseConfiguration(Environment env, CacheManager cacheManager) {
         this.env = env;
+        this.cacheManager = cacheManager;
     }
 
-    /**
-     * Open the TCP port for the H2 database, so it is available remotely.
-     *
-     * @return the H2 database TCP server
-     * @throws SQLException if the server failed to start
-     */
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
-    public Object h2TCPServer() throws SQLException {
-        String port = getValidPortForH2();
-        log.debug("H2 database is available on port {}", port);
-        return H2ConfigurationHelper.createServer(port);
-    }
-	
-    private String getValidPortForH2() throws NumberFormatException {
-        int port = Integer.parseInt(env.getProperty("server.port"));
-        if (port < 10000) {
-            port = 10000 + port;
+    @Bean
+    public SpringLiquibase liquibase(@Qualifier("taskExecutor") TaskExecutor taskExecutor,
+                                     DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+
+        // Use liquibase.integration.spring.SpringLiquibase if you don't want Liquibase to start asynchronously
+        SpringLiquibase liquibase = new AsyncSpringLiquibase(taskExecutor, env);
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        if (env.acceptsProfiles(JHipsterConstants.SPRING_PROFILE_NO_LIQUIBASE)) {
+            liquibase.setShouldRun(false);
         } else {
-            if (port < 63536) {
-                port = port + 2000;
-            } else {
-                port = port - 2000;
-            }
+            liquibase.setShouldRun(liquibaseProperties.isEnabled());
+            log.debug("Configuring Liquibase");
         }
-        return String.valueOf(port);
+        return liquibase;
     }
 }
